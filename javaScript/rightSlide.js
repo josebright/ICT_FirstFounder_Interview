e5a32daf1a7451e63d8348a3024282f6e4390f26
Ugwumadu Uchenna Bright <josebright29@gmail.com>
2020-07-28 20:52:23 +0100

    Update rightSlide.js

--- javaScript/rightSlide.js
+++ javaScript/rightSlide.js
@@ -1,24 +1,46 @@
 
-// code for click and slide right function
+// converted the class to selector 
 var bar = document.querySelector(".bar");
 var aside = document.querySelector(".aside");
 var main = document.querySelector(".main");
 var direction = document.querySelector(".direction");
+var sideh = document.querySelector(".side > h1");
+var sidep = document.querySelector(".side > p");
 var side = document.querySelector(".side");
 
-document.getElementById("move").addEventListener("click", control);
+let checkbox = document.getElementById("move").addEventListener("click", control);   
 function control(){
-    bar.style.visibility = "hidden";
-    aside.style.width = "25vmax";
-    direction.style.width = "25vmax";
-    main.style.width = "75vmax";
-    side.style.width = "40vmax";
-    aside.style.transition = "all 1s";
-    bar.style.transition = "all 1s";
-    direction.style.transition = "all 1s";
-    main.style.transition = "all 1s";
-    
-    
-    return false;
-}
+    if (checkbox !== true){
 
+        bar.style.visibility = "hidden";
+        aside.style.width = "25%";
+        direction.style.right = "20rem";
+        main.style.width = "75%";
+        sideh.style.fontSize = "3.5vmax";
+        sidep.style.fontSize = "1.5vmax";
+        side.style.paddingTop = "18vmax";
+        sidep.style.transition = "all 1s";
+        sideh.style.transition = "all 1s";
+        bar.style.transition = "all 1s";
+        main.style.transition = "all 1s";
+        side.style.transition = "all 1s";
+
+        return true;
+
+    }else{
+        bar.style.visibility = "visible";
+        aside.style.width = "0";
+        direction.style.right = "2em";
+        main.style.width = "97%";
+        sideh.style.fontSize = "2vmax";
+        sidep.style.fontSize = "1vmax";
+        side.style.paddingTop = "21vmax";
+        sidep.style.transition = "all 1s";
+        sideh.style.transition = "all 1s";
+        bar.style.transition = "all 1s";
+        main.style.transition = "all 1.5s";
+        side.style.transition = "all 1s";
+
+        return false;
+    }
+}
